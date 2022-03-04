80698e532d87329e994ed9c55c43a80259babb23
rekket poslenovogodniy <daniilz2003@mail.ru>
2022-03-04 22:13:05 +0300

    Добавьте файлы проекта.

---
+++ LoginForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBase1
+{
+	public partial class LoginForm : Form
+	{
+		bool isLoginSuccessfull = false;
+		public LoginForm()
+		{
+			InitializeComponent();
+		}
+
+		private void label4_Click(object sender, EventArgs e)
+		{
+			RegistrationForm form = new RegistrationForm();
+			form.Show();
+		}
+
+		private void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (!isLoginSuccessfull)
+				Application.Exit();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\rekketio\source\repos\WindowsFormsApp2\Accounts.mdf;Integrated Security=True");
+			connection.Open();
+
+			string commandText = $"SELECT password FROM Accounts WHERE login = @login";
+
+			SqlCommand command = new SqlCommand(commandText, connection);
+			command.Parameters.Add("@login", SqlDbType.VarChar);
+			command.Parameters["@login"].Value = textBox1.Text;
+			byte[] hashedPassword;
+			using(SqlDataReader reader = command.ExecuteReader())
+			{
+				if (reader.Read())
+				{
+					hashedPassword = (byte[])reader.GetValue(0);
+					byte[] salt = new byte[16];
+					Array.Copy(hashedPassword, 0, salt, 0, 10);
+					var pbkdf2 = new Rfc2898DeriveBytes(textBox2.Text, salt, 100000);
+					byte[] hash = pbkdf2.GetBytes(20);
+					for (int i = 0; i < 20; i++)
+						if (hashedPassword[i+16] != hash[i])
+						{
+							MessageBox.Show("Incorrect username or password", "Authentication has failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+							connection.Close();
+							return;
+						}
+					MainForm form = new MainForm();
+					form.Show();
+					connection.Close();
+					this.Close();
+				} 
+				else
+				{
+					label3.Text = "Неверный логин или пароль";
+				}
+			}
+
+			connection.Close();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBase1
+{
+	static class Program
+	{
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			LoginForm form = new LoginForm();
+			form.Show();
+			Application.Run();
+		}
+	}
+}
