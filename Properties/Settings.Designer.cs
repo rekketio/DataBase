1ea6e527df1b172bb83babdb579aed00234d9b85
rekketio <rekketio@gmail.com>
2022-03-10 04:47:02 +0300

    .

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace WindowsFormsApp2.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Data\\DataBase" +
-            "s\\AutoShow.mdf;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Data\\AutoShow" +
+            ".mdf;Integrated Security=True")]
         public string AccountsConnectionString {
             get {
                 return ((string)(this["AccountsConnectionString"]));
